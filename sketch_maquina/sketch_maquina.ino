1deb23a24778c8d54d08b6fbd0db9747fe08b973
heitorFS <heitor.freitassilva@oulook.com>
2025-03-11 13:27:54 -0300

    adição de webserver

--- sketch_maquina/sketch_maquina.ino
+++ sketch_maquina/sketch_maquina.ino
@@ -1,7 +1,18 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+#define ssid "SmartHome"
+#define password "sae668128"
+
+ESP8266WebServer server(80);
+
 // Protótipos
 
-void (*MaquinaStep)(void);
+void handleHomePage(void);
+void handleNotFound(void);
 
+void (*MaquinaStep)(void);
 void StepInicial(void);
 void StepUm(void);
 
@@ -19,10 +30,40 @@ void StepUm(void)
 
 void setup()
 {
+  Serial.begin(9600);
   MaquinaStep = StepInicial;
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(400);
+  }
+  Serial.print("IP obtido: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleHomePage);
+  server.onNotFound(handleNotFound);
+
+  server.begin();
 }
 
 void loop()
 {
   (*MaquinaStep)();
+  server.handleClient();
+}
+
+void handleHomePage(void)
+{
+  String html = "<h1>Teste</h1>";
+  server.send(200, "text/html", html);
+}
+
+void handleNotFound(void)
+{
+  String html = "<h1>Não</h1>";
+  server.send(200, "text/html", html);
 }
